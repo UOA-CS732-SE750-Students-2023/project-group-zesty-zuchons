25a3eee9780c0499ecd64a545250eb7717e7488f
sherry wen <rikka9816@gmail.com>
2023-03-27 19:37:47 +1300

    Adjust style of Text Component
    now support hover and selected (focused) style

--- src/components/webCrafter/pageComponents/userModified/Text/Text.tsx
+++ src/components/webCrafter/pageComponents/userModified/Text/Text.tsx
@@ -7,7 +7,7 @@ import { Slider, FormControl, FormLabel } from "@mui/material";
 
 import componentDefaultStyle from "../../componentDefaultStyle.js";
 
-export const Text = ({ text, fontSize }) => {
+export const Text = ({ text, fontSize, padding }) => {
   const {
     connectors: { connect, drag },
     isActive,
@@ -24,10 +24,7 @@ export const Text = ({ text, fontSize }) => {
   }, [isActive]);
 
   return (
-    <div
-      ref={(ref) => connect(drag(ref))}
-      onClick={(e) => setEditable(true)}
-    >
+    <div ref={(ref) => connect(drag(ref))} onClick={(e) => setEditable(true)}>
       <ContentEditable
         onMouseEnter={() => {
           setHover(true);
@@ -45,8 +42,9 @@ export const Text = ({ text, fontSize }) => {
         }
         style={{
           fontSize: `${fontSize}px`,
+          padding: `${padding}px`,
           ...(hover ? componentDefaultStyle.componentHover : null),
-          ...(editable ? componentDefaultStyle.componentFocus : null)
+          ...(editable ? componentDefaultStyle.componentFocus : null),
         }}
       />
     </div>
@@ -57,8 +55,10 @@ const TextSettings = () => {
   const {
     actions: { setProp },
     fontSize,
+    padding
   } = useNode((node) => ({
     fontSize: node.data.props.fontSize,
+    padding: node.data.props.padding
   }));
 
   return (
@@ -67,11 +67,23 @@ const TextSettings = () => {
         <FormLabel component="legend">Font size</FormLabel>
         <Slider
           value={fontSize || 7}
-          step={7}
+          step={1}
           min={1}
           max={50}
           onChange={(_, value) => {
             setProp((props) => (props.fontSize = value));
+          }}
+        />
+      </FormControl>
+      <FormControl size="small" component="fieldset">
+        <FormLabel component="legend">Padding</FormLabel>
+        <Slider
+          value={padding || 10}
+          step={1}
+          min={1}
+          max={20}
+          onChange={(_, value) => {
+            setProp((props) => (props.padding = value));
           }}
         />
       </FormControl>
@@ -83,9 +95,7 @@ Text.craft = {
   props: {
     text: "Hi",
     fontSize: 20,
-  },
-  rules: {
-    canDrag: (node) => node.data.props.text != "Drag",
+    padding: 10,
   },
   related: {
     settings: TextSettings,
