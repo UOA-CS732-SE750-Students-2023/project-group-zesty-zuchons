fff0c2894045720b08461795a42d443b5199e452
sherry wen <rikka9816@gmail.com>
2023-03-31 20:06:29 +1300

    add logout button to craftpage header

--- src/components/webCrafter/editor/Header.tsx
+++ src/components/webCrafter/editor/Header.tsx
@@ -2,24 +2,31 @@ import React from "react";
 import { useEditor } from "@craftjs/core";
 
 import { Box, Grid, Button } from "@mui/material";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 import SaveAltIcon from "@mui/icons-material/SaveAlt";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
   const { actions, query, enabled } = useEditor((state) => ({
     enabled: state.options.enabled,
   }));
+  const navigate = useNavigate();
   function saveCanvasToServer() {
     //TODO: save the serialized json of current canvas to backend
     console.log(query.serialize());
   }
+  function logout() {
+    navigate("/");
+  }
 
   const TopbarButton = styled(Button)({
-    '&:hover':{
-      backgroundColor: '#bbdefb',
-    }
-  })
-  
+    "&:hover": {
+      backgroundColor: "#bbdefb",
+    },
+  });
+
   return (
     <Box className="header" sx={{ borderBottom: 1, borderColor: "grey.300" }}>
       <Grid container alignItems="left">
@@ -35,9 +42,17 @@ export const Header = () => {
           size="small"
           startIcon={<SaveAltIcon />}
           onClick={saveCanvasToServer}
-          style={{ position: "absolute", right: "5px" }}
+          style={{ position: "absolute", right: "100px" }}
         >
           Save
+        </TopbarButton>
+        <TopbarButton
+          size="small"
+          startIcon={<LogoutIcon />}
+          onClick={logout}
+          style={{ position: "absolute", right: "0" }}
+        >
+          Logout
         </TopbarButton>
       </Grid>
     </Box>
