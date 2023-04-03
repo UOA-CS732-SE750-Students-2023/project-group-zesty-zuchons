0a408342b71bb0b345e858930940f2559a50023d
Ziqi Zhong <zhongziqi-rh@adbc.com>
2023-04-03 17:41:34 +1200

    database created, fixed server.js - ziqi

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ app.use(cors());
 //Setting up routes
 app.use("/", routes);
 
+//database server address "mongodb+srv://zzq961213:Aa12345678@cluster0.5ewkh2k.mongodb.net/database"
 mongoose
-  .connect(`${process.env.MONGO_DB_URI}`, { useNewUrlParser: true })
+  .connect('mongodb+srv://zzq961213:Aa12345678@cluster0.5ewkh2k.mongodb.net/database', { useNewUrlParser: true })
   .then((result) => {
     console.log("MongoDB connection successful");
   })
@@ -28,3 +29,40 @@ mongoose
   .catch((error) => {
     console.log("MongoDB connection failed : ", error.message);
   });
+
+
+//user_infos entity defined
+const Schema = mongoose.Schema;
+//the some of the variable name are not defined
+const userSchema = new Schema({
+  userName:String,
+  userData:{
+      documentName:String,
+      test1:String,
+      test2:String
+  }
+});
+const user_info = mongoose.model('user_info', userSchema);
+
+//createUserInfo
+app.get('/createUserInfo', (req, res) => {
+  //use the test data to test back-end server connection
+  createUserInfo(insert);
+  res.send('Creation finished!');
+  });
+
+async function createUserInfo(userInfo){
+  const newUserInfo = new user_info(userInfo);
+  console.log("Start to create new user")
+  await newUserInfo.save();
+  console.log("Creation finished!")
+  return newUserInfo;
+};
+
+//this is a test input of creating user info
+const insert = {userName: 'test2',userData:{
+  documentName:'2222',
+  test1:'2222',
+  test2:'4444'
+}
+}
