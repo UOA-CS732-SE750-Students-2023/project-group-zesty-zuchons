362ad44440a18dfd453fbae8c45ff2163c1b8fc1
sherry wen <rikka9816@gmail.com>
2023-04-18 15:19:10 +1200

    add: add margin and padding setting for materialSwitch

--- src/components/webCrafter/pageComponents/muiElements/MaterialSwitch/MaterialSwitch.tsx
+++ src/components/webCrafter/pageComponents/muiElements/MaterialSwitch/MaterialSwitch.tsx
@@ -11,12 +11,21 @@ import {
   FormControlLabel,
   TextField,
   Divider,
-  Chip
+  Chip,
+  Slider
 } from "@mui/material";
 
 import componentDefaultStyle from "../../componentDefaultStyle.js";
+import store from "../../../../../store/store";
 
-export const MaterialSwitch = ({ label, color, labelPlacement, size }) => {
+export const MaterialSwitch = ({
+  label,
+  color,
+  labelPlacement,
+  size,
+  padding,
+  margin,
+}) => {
   const {
     // declare connector in useNode() to enable drag for the component
     connectors: { connect, drag },
@@ -25,13 +34,31 @@ export const MaterialSwitch = ({ label, color, labelPlacement, size }) => {
     isActive: node.events.selected,
   }));
 
+  const [hover, setHover] = useState(false);
+  let canvasEditable = store.getState();
+
   return (
-    // 完善下方setting panel时需同时将prop挂载到主元素上
     <div ref={(ref) => connect(drag(ref))}>
       <FormControlLabel
+        onMouseEnter={() => {
+          setHover(true);
+        }}
+        onMouseLeave={() => {
+          setHover(false);
+        }}
         control={<Switch size={size} color={color} />}
         label={label}
         labelPlacement={labelPlacement}
+        style={{
+          padding: `${padding}px`,
+          margin: `${margin}px`,
+          ...(hover && canvasEditable
+            ? componentDefaultStyle.componentHover
+            : null),
+          ...(isActive && canvasEditable
+            ? componentDefaultStyle.componentFocus
+            : null),
+        }}
       />
     </div>
   );
@@ -45,16 +72,20 @@ const MaterialSwitchSettings = () => {
     labelPlacement,
     color,
     size,
+    padding,
+    margin,
   } = useNode((node) => ({
     props: node.data.props,
     size: node.data.props.size,
     label: node.data.props.label,
     labelPlacement: node.data.props.labelPlacement,
     color: node.data.props.color,
+    padding: node.data.props.padding,
+    margin: node.data.props.margin,
   }));
   return (
     <div>
-      <Divider textAlign="left" style={{ paddingTop: "20px" }} color="#e0e0e0">
+      <Divider textAlign="left" color="#e0e0e0">
         <Chip size="small" variant="outlined" color="primary" label="props" />
       </Divider>
       <Typography component="div" variant="body1" mt={1}>
@@ -122,6 +153,42 @@ const MaterialSwitchSettings = () => {
           </Select>
         </FormControl>
       </Typography>
+      <Divider textAlign="left" style={{ paddingTop: "20px" }} color="#e0e0e0">
+        <Chip size="small" variant="outlined" color="primary" label="styles" />
+      </Divider>
+
+      <Typography component="div" variant="body1" mt={1}>
+        <FormControl size="small" component="fieldset" fullWidth>
+          <FormLabel component="legend">Padding</FormLabel>
+          <Slider
+            style={componentDefaultStyle.settingPanelSlide}
+            value={padding || 10}
+            step={1}
+            min={1}
+            max={20}
+            valueLabelDisplay="auto"
+            onChange={(_, value) => {
+              setProp((props) => (props.padding = value));
+            }}
+          />
+        </FormControl>
+      </Typography>
+      <Typography component="div" variant="body1" mt={1}>
+        <FormControl size="small" component="fieldset" fullWidth>
+          <FormLabel component="legend">Margin</FormLabel>
+          <Slider
+            style={componentDefaultStyle.settingPanelSlide}
+            value={margin || 5}
+            step={1}
+            min={1}
+            max={20}
+            valueLabelDisplay="auto"
+            onChange={(_, value) => {
+              setProp((props) => (props.margin = value));
+            }}
+          />
+        </FormControl>
+      </Typography>
     </div>
   );
 };
@@ -134,6 +201,8 @@ MaterialSwitch.craft = {
     labelPlacement: "end",
     label: "hi",
     color: "primary",
+    padding: 10,
+    margin: 5,
   },
   related: {
     settings: MaterialSwitchSettings,
